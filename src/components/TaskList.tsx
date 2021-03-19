3dab34a8be0256ed517f0af546801408a7611f90
Kalil Peixoto <kalilmvp@gmail.com>
2021-03-19 19:06:16 +0100

    :tada: Challenge 01 completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,31 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
+    if (!newTaskTitle) return;
+
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    setTasks([...tasks, {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }]);
+
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map((task: Task) => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task));
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const tasksFiltered = tasks.filter(task => task.id !== id);
+
+    setTasks(tasksFiltered);
   }
 
   return (
